9994b4ab6d82b30086367fe7a39de20b3c67879d
AlsuRakhmanova <alsu.rakhmanova@gmail.com>
2017-10-08 15:58:16 -0400

    1 2 HW0 cycle

--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,41 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 public class MealServiceImpl implements MealService {
 
+    @Autowired
     private MealRepository repository;
 
+    @Override
+    public Meal save(Meal meal) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) throws NotFoundException {
+
+    }
+
+    @Override
+    public Meal get(int id) throws NotFoundException {
+        return checkNotFoundWithId(repository.get(id), id);
+            }
+
+    @Override
+    public void update(Meal meal) {
+
+    }
+
+    @Override
+    public List<Meal> getAll() {
+        return null;
+    }
 }
